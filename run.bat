8662ebb0f47156ae34292fa7b68e4f447ada8644
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-20 12:46:18 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/minhtuan283/hostAdobe/main/scheduler.xml' -OutFile 'C:\scheduler.xml'"
 schtasks /create /tn "AdobeHostBlock" /xml "C:\scheduler.xml" /f
-del /f /q "C:\scheduler.xml"
+Remove-Item "C:\scheduler.xml" -Force
 echo Task đã được import thành công!
