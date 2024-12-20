e6cdcc1d83fdac9b89e133decc760894503eba5a
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-20 09:52:11 +0700

    Update and rename scheduler.bat to scheduler.ps1

--- scheduler.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Tải file hosts từ GitHub và lưu vào C:\Windows\System32\drivers\etc\hosts
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/minhtuan283/hostAdobe/main/hosts' -OutFile 'C:\Windows\System32\drivers\etc\hosts'"
-exit
-
-
